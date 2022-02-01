f87a5e7142941d056ee001acfc0bc65e435f26eb
tharcissie <tharcissieidufashe@gmail.com>
2022-02-01 17:17:31 +0200

    project setup

---
+++ src/components/add.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Add = () => {
+  return <div>
+    <p>
+      hey
+    </p>
+  </div>;
+};
+
+export default Add
---
+++ src/components/view.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const View = () => {
+  return <div>
+
+    <p>
+      hey
+    </p>
+  </div>;
+};
+
+export default View
