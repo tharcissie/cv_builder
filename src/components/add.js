52eeefbcc2881624d5a7f64d8f4baf1b534dcef6
tharcissie <tharcissieidufashe@gmail.com>
2022-02-02 14:46:11 +0200

    implemented sharing

--- src/components/add.js
+++ src/components/add.js
@@ -3,8 +3,7 @@ import { useNavigate } from "react-router-dom";
 const Add = () => {
   const navigate = useNavigate();
 
-  const [input, setInputs] = useState({ email: '', names: '', phone: '', linkedin: '', school: '', course: '', start: '', end: '', job: '', company: '', enter: '', leave: '', school1: '', course1: '', start1: '', end1: '', job1: '', company1: '', enter1: '', leave1: '' })
-
+  const [input, setInputs] = useState({ email: '', names: '', phone: '', linkedin: '', school: '', course: '', start: '', end: '', job: '', company: '', enter: '', leave: '', school1: '', course1: '', start1: '', end1: '', job1: '', company1: '', enter1: '', leave1: '', des1: '', des2: '', des3: '', des4: '', summary: '' })
   const handleChange = (e) => {
     const value = e.target.value;
     setInputs({
@@ -16,116 +15,82 @@ const Add = () => {
   const handlesubmit = (e) => {
     e.preventDefault()
     try {
-      window.localStorage.setItem("cv", JSON.stringify({ input}))
+      window.localStorage.setItem("cv", JSON.stringify({ input }))
     } catch (error) {
       console.log(error)
     }
-    setInputs({ email: '', names: '', phone: '', linkedin: '', school: '', course: '', start: '', end: '', job: '', company: '', enter: '', leave: '', school1: '', course1: '', start1: '', end1: '', job1: '', company1: '', enter1: '', leave1: '' });
+    setInputs({ email: '', names: '', phone: '', linkedin: '', school: '', course: '', start: '', end: '', job: '', company: '', enter: '', leave: '', school1: '', course1: '', start1: '', end1: '', job1: '', company1: '', enter1: '', leave1: '', des1: '', des2: '', des3: '', des4: '', summary: '' });
     navigate('/view')
   }
-  
 
+ 
 
   return (<div className="container mt-5">
 
     <h1 className="text-center">CV BUILDER</h1>
     <div className="card p-4">
-    <form className="mt-5" onSubmit={handlesubmit}>
-      <div className="row justify-content-center">
-        <div className="col-md-4">
-          <h4>Personal Details</h4>
-          <div className=" mt-2  p-2">
-            <div className="form-group">
-              <input
-                name="names"
-                value={input.names || ""}
-                onChange={handleChange}
-                type="text"
-                className="form-control"
-                placeholder="Enter First and Last Name..." />
-            </div>
-            <div className="form-group">
-              <input
-                name="phone"
-                value={input.phone || ""}
-                onChange={handleChange}
-                type="number"
-                className="form-control"
-                placeholder="Enter Phone Number..." />
-            </div>
-            <div className="form-group">
-              <input
-                name="email"
-                value={input.email || ""}
-                onChange={handleChange}
-                type="email"
-                className="form-control"
-                placeholder="Enter Email Address..." />
-            </div>
-            <div className="form-group">
-              <input
-                name="linkedin"
-                value={input.linkedin || ""}
-                onChange={handleChange}
-                type="text"
-                className="form-control"
-                placeholder="Enter LinkedLn Url..." />
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <h4>Educational Backgroound</h4>
-          <div className=" mt-2  p-2">
-            <div className="form-group">
-              <input
-                name="school"
-                value={input.school || ""}
-                onChange={handleChange}
-                type="text"
-                className="form-control"
-                placeholder="Enter Institution Name..." />
-            </div>
-            <div className="form-group">
-              <input
-                name="course"
-                value={input.course || ""}
-                onChange={handleChange}
-                type="text"
-                className="form-control"
-                placeholder="Enter Course Name..." />
-            </div>
-            <div className="row">
-              <div className="col-md-6">
-                <div className="form-group">
-                  <label >Start Date</label>
-                  <input
-                    name="start"
-                    value={input.start || ""}
-                    onChange={handleChange}
-                    type="date"
-                    className="form-control" />
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="form-group">
-                  <label >End Date</label>
-                  <input
-                    name="end"
-                    value={input.end || ""}
-                    onChange={handleChange}
-                    type="date"
-                    className="form-control" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <button className=" btn btn-sm btn-info" type="button" data-toggle="collapse" data-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample1">Add Education</button>
-          <div className="collapse" id="collapseExample1">
+      <form className="mt-5" onSubmit={handlesubmit}>
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <h4>Personal Details</h4>
             <div className=" mt-2  p-2">
               <div className="form-group">
                 <input
-                  name="school1"
-                  value={input.school1 || ""}
+                  name="names"
+                  value={input.names || ""}
+                  onChange={handleChange}
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter First and Last Name..." />
+              </div>
+              <div className="form-group">
+                <input
+                  name="phone"
+                  value={input.phone || ""}
+                  onChange={handleChange}
+                  type="number"
+                  className="form-control"
+                  placeholder="Enter Phone Number..." />
+              </div>
+              <div className="form-group">
+                <input
+                  name="email"
+                  value={input.email || ""}
+                  onChange={handleChange}
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter Email Address..." />
+              </div>
+              <div className="form-group">
+                <input
+                  name="linkedin"
+                  value={input.linkedin || ""}
+                  onChange={handleChange}
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter LinkedLn Url..." />
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleFormControlTextarea1">Summary</label>
+                <textarea
+                  className="form-control"
+                  id="exampleFormControlTextarea1"
+                  rows="3"
+                  name="summary"
+                  value={input.summary || ""}
+                  onChange={handleChange}
+                >
+                </textarea>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <h4>Educational Backgroound</h4>
+            <div className=" mt-2  p-2">
+              <div className="form-group">
+                <input
+                  name="school"
+                  value={input.school || ""}
                   onChange={handleChange}
                   type="text"
                   className="form-control"
@@ -133,8 +98,8 @@ const Add = () => {
               </div>
               <div className="form-group">
                 <input
-                  name="course1"
-                  value={input.course1 || ""}
+                  name="course"
+                  value={input.course || ""}
                   onChange={handleChange}
                   type="text"
                   className="form-control"
@@ -145,8 +110,8 @@ const Add = () => {
                   <div className="form-group">
                     <label >Start Date</label>
                     <input
-                      name="start1"
-                      value={input.start1 || ""}
+                      name="start"
+                      value={input.start || ""}
                       onChange={handleChange}
                       type="date"
                       className="form-control" />
@@ -156,72 +121,94 @@ const Add = () => {
                   <div className="form-group">
                     <label >End Date</label>
                     <input
-                      name="end1"
-                      value={input.end1 || ""}
+                      name="end"
+                      value={input.end || ""}
                       onChange={handleChange}
                       type="date"
                       className="form-control" />
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <h4>Experience</h4>
-          <div className=" mt-2  p-2">
-            <div className="form-group">
-              <input
-                name="job"
-                value={input.job || ""}
-                onChange={handleChange}
-                type="text"
-                className="form-control"
-                placeholder="Enter Job Title..." />
-            </div>
-            <div className="form-group">
-              <input
-                name="company"
-                value={input.company || ""}
-                onChange={handleChange}
-                type="text"
-                className="form-control"
-                placeholder="Enter Company Name..." />
-            </div>
-            <div className="row">
-              <div className="col-md-6">
                 <div className="form-group">
-                  <label >Start Date</label>
-                  <input
-                    name="enter"
-                    value={input.enter || ""}
-                    onChange={handleChange}
-                    type="date"
+                  <label htmlFor="exampleFormControlTextarea1">Description</label>
+                  <textarea
                     className="form-control"
-                    placeholder="Enter Start Date..." />
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="form-group">
-                  <label >End Date</label>
-                  <input
-                    name="leave"
-                    value={input.leave || ""}
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                    name="des1"
+                    value={input.des1 || ""}
                     onChange={handleChange}
-                    type="date"
-                    className="form-control"
-                    placeholder="Enter End Date..." />
+                  >
+                  </textarea>
                 </div>
               </div>
             </div>
+            <button className=" btn btn-sm btn-info" type="button" data-toggle="collapse" data-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample1">Add Education</button>
+            <div className="collapse" id="collapseExample1">
+              <div className=" mt-2  p-2">
+                <div className="form-group">
+                  <input
+                    name="school1"
+                    value={input.school1 || ""}
+                    onChange={handleChange}
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Institution Name..." />
+                </div>
+                <div className="form-group">
+                  <input
+                    name="course1"
+                    value={input.course1 || ""}
+                    onChange={handleChange}
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Course Name..." />
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label >Start Date</label>
+                      <input
+                        name="start1"
+                        value={input.start1 || ""}
+                        onChange={handleChange}
+                        type="date"
+                        className="form-control" />
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label >End Date</label>
+                      <input
+                        name="end1"
+                        value={input.end1 || ""}
+                        onChange={handleChange}
+                        type="date"
+                        className="form-control" />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="exampleFormControlTextarea1">Description</label>
+                    <textarea
+                      className="form-control"
+                      id="exampleFormControlTextarea1"
+                      rows="3"
+                      name="des2"
+                      value={input.des2 || ""}
+                      onChange={handleChange}
+                    >
+                    </textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <button className=" btn btn-sm btn-info" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Add Experience</button>
-          <div className="collapse" id="collapseExample">
-            <div className="p-2  mt-2 ">
+          <div className="col-md-4">
+            <h4>Experience</h4>
+            <div className=" mt-2  p-2">
               <div className="form-group">
                 <input
-                  name="job1"
-                  value={input.job1 || ""}
+                  name="job"
+                  value={input.job || ""}
                   onChange={handleChange}
                   type="text"
                   className="form-control"
@@ -229,8 +216,8 @@ const Add = () => {
               </div>
               <div className="form-group">
                 <input
-                  name="company1"
-                  value={input.company1 || ""}
+                  name="company"
+                  value={input.company || ""}
                   onChange={handleChange}
                   type="text"
                   className="form-control"
@@ -241,8 +228,8 @@ const Add = () => {
                   <div className="form-group">
                     <label >Start Date</label>
                     <input
-                      name="enter1"
-                      value={input.enter1 || ""}
+                      name="enter"
+                      value={input.enter || ""}
                       onChange={handleChange}
                       type="date"
                       className="form-control"
@@ -253,24 +240,96 @@ const Add = () => {
                   <div className="form-group">
                     <label >End Date</label>
                     <input
-                      name="leave1"
-                      value={input.leave1 || ""}
+                      name="leave"
+                      value={input.leave || ""}
                       onChange={handleChange}
                       type="date"
                       className="form-control"
                       placeholder="Enter End Date..." />
                   </div>
                 </div>
+                <div className="form-group">
+                  <label htmlFor="exampleFormControlTextarea1">Description</label>
+                  <textarea
+                    className="form-control"
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                    name="des3"
+                    value={input.des3 || ""}
+                    onChange={handleChange}
+                  >
+                  </textarea>
+                </div>
+              </div>
+            </div>
+            <button className=" btn btn-sm btn-info" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Add Experience</button>
+            <div className="collapse" id="collapseExample">
+              <div className="p-2  mt-2 ">
+                <div className="form-group">
+                  <input
+                    name="job1"
+                    value={input.job1 || ""}
+                    onChange={handleChange}
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Job Title..." />
+                </div>
+                <div className="form-group">
+                  <input
+                    name="company1"
+                    value={input.company1 || ""}
+                    onChange={handleChange}
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Company Name..." />
+                </div>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label >Start Date</label>
+                      <input
+                        name="enter1"
+                        value={input.enter1 || ""}
+                        onChange={handleChange}
+                        type="date"
+                        className="form-control"
+                        placeholder="Enter Start Date..." />
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <label >End Date</label>
+                      <input
+                        name="leave1"
+                        value={input.leave1 || ""}
+                        onChange={handleChange}
+                        type="date"
+                        className="form-control"
+                        placeholder="Enter End Date..." />
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="exampleFormControlTextarea1">Description</label>
+                    <textarea
+                      className="form-control"
+                      id="exampleFormControlTextarea1"
+                      rows="3"
+                      name="des4"
+                      value={input.des4 || ""}
+                      onChange={handleChange}
+                    >
+                    </textarea>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+          <div className="text-center mt-5">
+            <button type="submit" className="btn btn-info mt-3 ">BUILD YOUR CV</button>
+          </div>
         </div>
-      </div>
-      <div className="text-center mt-5">
-      <button type="submit" className="btn btn-info mt-3 ">BUILD YOUR CV</button>
-      </div>
-    </form >
-    </div>
+      </form >
+    </div >
   </div >)
 };
 
--- src/components/view.js
+++ src/components/view.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { jsPDF } from "jspdf";
 import html2canvas from 'html2canvas';
+import './css/style.css'
+import {
+  FacebookShareButton, FacebookIcon,
+  LinkedinShareButton, LinkedinIcon,
+  TwitterShareButton, TwitterIcon,
+  WhatsappShareButton, WhatsappIcon
+} from "react-share";
 
 const View = () => {
 
@@ -11,12 +18,12 @@ const View = () => {
         const imgData = canvas.toDataURL('image/png');
         const pdf = new jsPDF();
         pdf.addImage(imgData, 'JPEG', 0, 0);
-        // pdf.output('dataurlnewwindow');
         pdf.save("download.pdf");
       });
   }
 
-
+  const currentURL = window.location.href
+  console.log(currentURL)
 
   let data = JSON.parse(localStorage.getItem("cv"));
   const cv = data.input
@@ -26,21 +33,123 @@ const View = () => {
   return <div>
     <div className='container mt-5'>
       <div className='mb-2'>
-        <button className='btn btn-sm btn-success mr-2' onClick={() =>printDocument()}>Download</button>
-        <button className='btn btn-sm btn-info'>Share</button>
-      </div>
-      <div id='cv' className='card p-5'> <div className='row'>
-        <div className='col-md-7 p-2'>
-          <div className='align-center'>
-            <h1 style={{color:"blue"}}><b>{cv.names}</b></h1>
-            <p>{cv.email}</p>
-            <p>{cv.phone}</p>
-            <a href={cv.linkedin}>{cv.linkedin}</a>
+        <button className='btn btn-sm btn-info mr-2' onClick={() => printDocument()}><i className="fas fa-download"></i>Download</button>
+        <button className='btn btn-sm btn-success mr-2' data-toggle="modal" data-target="#exampleModal" ><i className="fas fa-share"></i>Share</button>
+
+        <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Share With</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <FacebookShareButton
+                className='mr-3'
+                  url={currentURL}>
+                  <FacebookIcon size={32} round />
+                </FacebookShareButton>
+
+                <TwitterShareButton
+                className='mr-3'
+                  url={currentURL}>
+                  <TwitterIcon size={32} round />
+                </TwitterShareButton>
+
+                <LinkedinShareButton
+                className='mr-3'
+                  url={currentURL}>
+                  <LinkedinIcon size={32} round />
+                </LinkedinShareButton>
+                <WhatsappShareButton
+                  url={currentURL}>
+                  <WhatsappIcon size={32} round />
+                </WhatsappShareButton>
+              </div>
+            </div>
           </div>
         </div>
-        <div className='col-md-5 p-2'>
-        </div>
       </div>
+      <div className='row justify-content-center'>
+        <div className='col-9'>
+          <div id='cv' className='card'>
+            <div className='row'>
+              <div className='col-md-7 p-5 '>
+                <div className='profile'>
+                  <h1 className='name'><b>{cv.names}</b></h1>
+                  <div className='ml-5'>
+                    <p><i className="far fa-envelope-open"></i> {cv.email}</p>
+                    <p><i className="fas fa-phone"></i> {cv.phone}</p>
+                    <a href={cv.linkedin}><i className="fab fa-linkedin text-dark"></i> {cv.linkedin}</a>
+                  </div>
+                </div>
+
+                <div className='work'>
+                  <h6 className='text-uppercase'><i className="fas fa-user-graduate"></i> <b>Educational Backgroound</b></h6>
+                  <div className='row mt-4'>
+                    <div className='col-4 date'>
+                      <p>{cv.enter} - {cv.leave}</p>
+                    </div>
+                    <div className='col-1'><i className="far fa-dot-circle"></i></div>
+                    <div className='col-7'>
+                      <p className='text-uppercase'><b>{cv.school}</b></p>
+                      <p> <small>{cv.course}</small></p>
+                      <p>{cv.des1}</p>
+
+                    </div>
+
+                  </div>
+                  {cv.school1 ? <div className='row mt-4'>
+                    <div className='col-4 date'>
+                      <p>{cv.enter1} - {cv.leave1}</p>
+                    </div>
+                    <div className='col-1'><i className="far fa-dot-circle"></i></div>
+                    <div className='col-7'>
+                      <p className='text-uppercase'><b>{cv.school1}</b></p>
+                      <p> <small>{cv.course1}</small></p>
+                      <p>{cv.des2}</p>
+                    </div>
+                  </div> : null}
+                </div>
+
+                <div className='experience mt-5'>
+                  <h6 className='text-uppercase'><i className="fas fa-user-graduate"></i> <b>Experience</b></h6>
+                  <div className='row mt-4'>
+                    <div className='col-4 date'>
+                      <p>{cv.start} - {cv.end}</p>
+                    </div>
+                    <div className='col-1'><i className="far fa-dot-circle"></i></div>
+                    <div className='col-7'>
+                      <p className='text-uppercase'><b>{cv.company}</b></p>
+                      <p> <small>{cv.job}</small></p>
+                      <p>{cv.des3}</p>
+                    </div>
+
+                  </div>
+                  {cv.company1 ? <div className='row mt-4'>
+                    <div className='col-4 date'>
+                      <p>{cv.start1} - {cv.end1}</p>
+                    </div>
+                    <div className='col-1'><i className="far fa-dot-circle"></i></div>
+                    <div className='col-7'>
+                      <p className='text-uppercase'><b>{cv.company1}</b></p>
+                      <p> <small>{cv.job1}</small></p>
+                      <p>{cv.des4}</p>
+                    </div>
+                  </div> : null}
+                </div>
+
+              </div>
+              <div className='col-md-5 p-5 left'>
+                <i className="fas fa-quote-left quote"></i>
+                <p className='p-4'>{cv.summary}</p>
+                <i className="fas fa-quote-right quote"></i>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>;
